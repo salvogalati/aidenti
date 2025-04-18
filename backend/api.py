d74bb5a3f6b6ccc5ed0423ab8aa0f30beb650854
salvogalati <sgalati95@yahoo.it>
2025-04-18 15:34:51 +0200

    feat: implement user authentication and registration endpoints

--- backend/api.py
+++ backend/api.py
@@ -1,23 +1,54 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from db import user_exists, check_credentials, add_user
 
 app = Flask(__name__)
 
 CORS(app)
 
-# Endpoint per ottenere tutti i messaggi
-@app.route('/messaggi', methods=['GET'])
-def get_messaggi():
-    return jsonify("CIAOOO!")
 
-# Endpoint base
+@app.route('/test_get', methods=['GET'])
+def get_messaggi():
+    return jsonify("This is a Test GET API!"), 200
+
+@app.route('/test_post', methods=['POST'])
+def test_post():
+    print("Headers:", dict(request.headers))
+    print("Query Params:", request.args.to_dict())
+    
+    if request.is_json:
+        print("JSON Body:", request.get_json())
+    else:
+        print("Form Data:", request.form.to_dict())
+
+    return {"status": "received"}, 200
+
 @app.route('/')
 def index():
-    return "<h1>API CIAOC con Flask!</h1>"
+    return "<h1>Home page!</h1>"
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if check_credentials(username, password):
+        return jsonify({'message': 'Login successful'}), 200
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if user_exists(username):
+        return jsonify({'message': 'Username already exists'}), 409
+
+    add_user(username, password)
+    return jsonify({'message': 'User registered successfully'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
