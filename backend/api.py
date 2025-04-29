626b608da065d3602dfca56b539828d9bbe73215
salvogalati <sgalati95@yahoo.it>
2025-04-29 23:36:17 +0200

    feat: update user authentication to use email instead of username

--- backend/api.py
+++ backend/api.py
@@ -1,14 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from db import user_exists, check_credentials, add_user
+from db import user_exists, check_credentials, add_user, load_users
 
 app = Flask(__name__)
 
 CORS(app)
-
-
 @app.route('/test_get', methods=['GET'])
-def get_messaggi():
+def test_get():
     return jsonify("This is a Test GET API!"), 200
 
 @app.route('/test_post', methods=['POST'])
@@ -25,29 +23,31 @@ def test_post():
 
 @app.route('/')
 def index():
-    return "<h1>Home page!</h1>"
+    return "<h1>Home page di Nicola-Salvatore API!</h1>"
+
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    username = data.get('username')
+    print(data)
+    email = data.get('email')
     password = data.get('password')
-
-    if check_credentials(username, password):
-        return jsonify({'message': 'Login successful'}), 200
+    check, message = check_credentials(email, password)
+    if check:
+        return jsonify({'message': message}), 200
     else:
-        return jsonify({'message': 'Invalid credentials'}), 401
+        return jsonify({'message': message}), 401
 
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    username = data.get('username')
+    email = data.get('email')
     password = data.get('password')
 
-    if user_exists(username):
-        return jsonify({'message': 'Username already exists'}), 409
+    if user_exists(email):
+        return jsonify({'message': 'Email already exists'}), 409
 
-    add_user(username, password)
+    add_user(email, password)
     return jsonify({'message': 'User registered successfully'}), 201
 
 if __name__ == '__main__':
