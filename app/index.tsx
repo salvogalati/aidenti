4ed0ed6f406f5d68c0e475a55f3f758319eea322
nicolamarmugi <nicola.marmugi@aigot.com>
2025-05-08 10:04:02 +0200

    feat: add bg

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Button, ButtonSpinner, ButtonText } from "@/components/ui/button";
 import { VStack } from "@/components/ui/vstack";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import React, { useEffect } from "react";
 import EmailInput from "./components/emailInput/emailInput";
 import PasswordInput from "./components/passwordInput/passwordInput";
@@ -57,73 +57,78 @@ export default function Auth() {
 	}
 
 	return (
-		<VStack className="w-full h-full justify-center items-center">
+		<VStack className="w-full h-full justify-center items-center relative">
 			{loading ? (
 				<>
 					<Image style={{ width: 320, height: 320 }} source={require('./assets/logoApp.png')} />
 					<Loader />
 				</>
 			) : (
-				<VStack className="w-full max-w-[300px] rounded-md border border-background-200 p-4 relative">
-					<Monster classMonster={`absolute top-3 right-0 max-w-52 max-h-52 ${hasErrors ? 'animate-hesitate' : 'animate-walk-and-flip'}`} />
-					<Text style={{ fontFamily: 'IndieFlower_400Regular' }} className="text-[30px] mb-2 font-semibold">
-						{isLoginPage ? "Log in" : "Sign in"}
-					</Text>
+				<>
+					<View className="absolute w-full h-full top-0 left-0">
+						<Image blurRadius={5} className="object-cover" style={{ width: '100%', height: '100%' }} source={require('./assets/backgroundLogin.png')} />
+					</View>
+					<VStack className="w-full max-w-[300px] rounded-md border border-background-200 p-4 relative bg-white">
+						<Monster classMonster={`absolute top-3 right-0 max-w-52 max-h-52 ${hasErrors ? 'animate-hesitate' : 'animate-walk-and-flip'}`} />
+						<Text style={{ fontFamily: 'IndieFlower_400Regular' }} className="text-[30px] mb-2 font-semibold">
+							{isLoginPage ? "Log in" : "Sign in"}
+						</Text>
 
-					<EmailInput
-						isInvalid={isInvalid}
-						inputValue={inputValue}
-						setInputValue={setInputValue}
-					/>
+						<EmailInput
+							isInvalid={isInvalid}
+							inputValue={inputValue}
+							setInputValue={setInputValue}
+						/>
 
-					<PasswordInput
-						isInvalid={isInvalid}
-						inputValue={inputValue}
-						setInputValue={setInputValue}
-						showPassword={showPassword}
-						handleState={() => handleState(setShowPassword)}
-					/>
-
-					{!isLoginPage && (
-						<ConfirmPasswordInput
+						<PasswordInput
 							isInvalid={isInvalid}
 							inputValue={inputValue}
 							setInputValue={setInputValue}
 							showPassword={showPassword}
 							handleState={() => handleState(setShowPassword)}
 						/>
-					)}
 
-					{
-						message &&
-						<Text className="text-end mt-2">
-							{message}
-						</Text>
-					}
+						{!isLoginPage && (
+							<ConfirmPasswordInput
+								isInvalid={isInvalid}
+								inputValue={inputValue}
+								setInputValue={setInputValue}
+								showPassword={showPassword}
+								handleState={() => handleState(setShowPassword)}
+							/>
+						)}
 
-					<Button
-						className={`w-fit self-end mt-4 ${!canSubmit || isSending ? "opacity-50" : ""}`}
-						size="sm"
-						onPress={() => handleSubmit(inputValue, isLoginPage, setIsInvalid, setMessage, setIsSending, router)}
-						disabled={!canSubmit || isSending}
-					>
 						{
-							isSending &&
-							<ButtonSpinner color={colors.gray[400]} />
+							message &&
+							<Text className="text-end mt-2">
+								{message}
+							</Text>
 						}
-						<ButtonText>Submit</ButtonText>
-					</Button>
 
-					<SwitchAuth
-						isLoginPage={isLoginPage}
-						setIsLoginPage={setIsLoginPage}
-						setInputValue={setInputValue}
-						setIsInvalid={setIsInvalid}
-						setShowPassword={setShowPassword}
-						setMessage={setMessage}
-						isSending={isSending}
-					/>
-				</VStack>
+						<Button
+							className={`w-fit self-end mt-4 ${!canSubmit || isSending ? "opacity-50" : ""}`}
+							size="sm"
+							onPress={() => handleSubmit(inputValue, isLoginPage, setIsInvalid, setMessage, setIsSending, router)}
+							disabled={!canSubmit || isSending}
+						>
+							{
+								isSending &&
+								<ButtonSpinner color={colors.gray[400]} />
+							}
+							<ButtonText>Submit</ButtonText>
+						</Button>
+
+						<SwitchAuth
+							isLoginPage={isLoginPage}
+							setIsLoginPage={setIsLoginPage}
+							setInputValue={setInputValue}
+							setIsInvalid={setIsInvalid}
+							setShowPassword={setShowPassword}
+							setMessage={setMessage}
+							isSending={isSending}
+						/>
+					</VStack>
+				</>
 			)}
 		</VStack>
 	);
