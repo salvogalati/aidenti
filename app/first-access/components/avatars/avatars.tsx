01ab1f5533510201a433cf6bfba050195bdde145
nicolamarmugi <nicola.marmugi@aigot.com>
2025-05-08 11:34:46 +0200

    feat: add GET for avatars

--- app/first-access/components/avatars/avatars.tsx
+++ app/first-access/components/avatars/avatars.tsx
@@ -2,20 +2,41 @@ import { View, Text, Pressable } from "react-native";
 import { Image } from "expo-image";
 import { HStack } from "@/components/ui/hstack";
 import IUsername from "@/first-access/types/username";
+import { useEffect, useState } from "react";
+import Loader from "@/components/loader/loader";
+import { VStack } from "@/components/ui/vstack";
 
-const avatars = [
-	{ id: '1', src: require('../../../../backend/avatars/Avatar1.png') },
-	{ id: '2', src: require('../../../../backend/avatars/Avatar2.png') },
-	{ id: '3', src: require('../../../../backend/avatars/Avatar3.png') },
-	{ id: '4', src: require('../../../../backend/avatars/Avatar4.png') },
-	{ id: '5', src: require('../../../../backend/avatars/Avatar5.png') },
-	{ id: '6', src: require('../../../../backend/avatars/Avatar6.png') },
-	{ id: '7', src: require('../../../../backend/avatars/Avatar7.png') },
-	{ id: '8', src: require('../../../../backend/avatars/Avatar8.png') },
-	{ id: '9', src: require('../../../../backend/avatars/Avatar9.png') },
-];
+interface IAvatar {
+	id: string;
+	src: string;
+}
 
 export default function Avatars({ payload, setPayload }: IUsername) {
+	const [avatars, setAvatars] = useState<IAvatar[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		const getAvatars = async () => {
+			try {
+				setIsLoading(true);
+				const res = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/api/avatar_images`);
+				const data = await res.json();
+				setAvatars(data);
+			} catch (e) {
+				console.error(e);
+			} finally {
+				setIsLoading(false);
+			}
+		}
+		getAvatars();
+	}, [])
+
+	if (isLoading) return (
+		<VStack className="justify-center items-center min-w-[266px] min-h-[285px]">
+			<Loader />
+		</VStack>
+	)
+
 	return (
 		<View>
 			<Text style={{ fontFamily: 'IndieFlower_400Regular' }} className="text-lg font-semibold">
@@ -28,7 +49,7 @@ export default function Avatars({ payload, setPayload }: IUsername) {
 						onPress={() => setPayload({ ...payload, avatar: avatar.id })}
 						className={`w-20 h-20 rounded-full overflow-hidden ${payload.avatar === avatar.id && 'border-2'}`}
 					>
-						<Image style={{ width: '100%', height: '100%' }} source={avatar.src} />
+						<Image style={{ width: '100%', height: '100%' }} source={{uri: avatar.src}} />
 					</Pressable>
 				))}
 			</HStack>
