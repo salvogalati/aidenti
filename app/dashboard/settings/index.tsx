22d2a690ff919e132bfa6d4ffb8911482d1a7fb6
Nicola Marmugi <marmu1@hotmail.it>
2025-05-14 21:37:18 +0200

    clean code

--- app/dashboard/settings/index.tsx
+++ app/dashboard/settings/index.tsx
@@ -1,29 +1,11 @@
-import { useContext } from "react";
-import { View, Text, Button } from "react-native";
-import { UserContext } from "../context/UserContext";
-import { Link } from "expo-router";
+import { View, Text } from "react-native";
 
 export default function Settings() {
-    const { user, setUser } = useContext(UserContext);
-
     return (
         <View>
             <Text>
-                {user?.name}
+                Settings
             </Text>
-            <Button
-                title="Modifica Nome"
-                onPress={() => {
-                    if (user) {
-                        setUser({ ...user, name: "Salvo" });
-                    }
-                }}
-            />
-            <Link href="/dashboard/jedo">
-                <Text>
-                    Torna alla Dashboard
-                </Text>
-            </Link>
         </View>
     );
 }
